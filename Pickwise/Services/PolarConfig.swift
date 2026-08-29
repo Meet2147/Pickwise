0bdc9f3f7e95600177ed4e4c7a4666259bb6d45a
Meet Jethwa <meetjethwa3@gmail.com>
2026-08-29 20:50:23 +0530

    Wire Polar checkout link into app + site; release build 5

--- Pickwise/Services/PolarConfig.swift
+++ Pickwise/Services/PolarConfig.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Public, non-secret Polar identifiers. Safe to commit.
 enum PolarConfig {
     static let organizationID = "36a24ca3-4af7-4c52-8fac-7243fb07019a"
-    static let checkoutURL = ""      // TODO: Polar checkout link for the Pickwise Pro subscription
+    static let checkoutURL = "https://buy.polar.sh/polar_cl_L0tIX7HRzlAE0Q3KzoqMQju5ZfDhSGXQOFxUA409HXp"
     static let price = "$5.99"
     static let period = "month"
     static let freeComparisons = 5
